4375e2c01d7b1aaaccda0227bf95e82455b80df4
Arpit Jain <aj070295@gmail.com>
2019-01-25 00:19:54 +0530

    registration part added

---
+++ data.py
@@ -0,0 +1,50 @@
+def Articles():
+
+	articles = [{
+
+		'id': 1,
+		'title': 'Article One',
+		'body' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod \
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo \
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse \
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non \
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+
+	'Author': 'Arpit Jain',
+	'created_date':'21-01-2019'
+	},
+	{
+		'id': 2,
+		'title': 'Article Two',
+		'body' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod \
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo \
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse \
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non \
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum',	
+
+	'Author': 'Manish sharma',
+	'created_date':'15-1-29019'
+
+	},
+
+	{
+	 'id': 3,
+		'title': 'Article Three',
+		'body' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod \
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo \
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse \
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non \
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+
+	'Author':'Rahul verma',
+	'created_date':'01-01-2019'
+	}
+
+
+
+	]
+
+	return articles
